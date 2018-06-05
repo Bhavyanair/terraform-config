56c19630dbcd1900202bfff4ab6bcfb5616dcada
root <root@jenkinstestserver.jzipz0o3rtcetgvjxuimt33jkd.tx.internal.cloudapp.net>
2018-06-05 09:36:55 +0000

    adding nic

--- main.tf
+++ main.tf
@@ -47,3 +47,14 @@ provider "azurerm" { }
     }
 }
 
+  resource "azurerm_network_interface" "nic" {
+  name                =  "terraformjenkinsnic"
+  resource_group_name =  "${azurerm_resource_group.terraformdemo.name}"
+ 
+  ip_configuration {
+        name                          =  "terraformNicConfig
+        subnet_id                     =  "${azurerm_subnet.subnet.id}"
+        private_ip_address_allocation =  "static"
+  }
+
+}
