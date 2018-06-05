6cace5d8a4aca202fe283db38e9e99f23869a7d8
root <root@jenkinstestserver.jzipz0o3rtcetgvjxuimt33jkd.tx.internal.cloudapp.net>
2018-06-05 09:00:13 +0000

    nsg

--- main.tf
+++ main.tf
@@ -29,5 +29,22 @@ provider "azurerm" { }
 
 }
 
+  resource "azurerm_network_security_group" "nsg" {
+  name                = "terraformjenkinsnsg"
+  location            = "eastus"
+  resource_group_name = "${azurerm_resource_group.terraformdemo.name}"
+
+  security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+}
 
 
