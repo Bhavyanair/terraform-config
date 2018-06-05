b5a6e0d770153e15bbc7d540f73d756751e8868c
root <root@jenkinstestserver.jzipz0o3rtcetgvjxuimt33jkd.tx.internal.cloudapp.net>
2018-06-05 07:02:23 +0000

    adding vnet

--- main.tf
+++ main.tf
@@ -6,3 +6,10 @@ provider "azurerm" { }
   location = "eastus"
 }
 
+  resource "azurerm_virtual_network" "network" {
+  name                = "terraformjenkinsnetwork"
+  address_space       = ["172.50.0.0/16"]
+  location            = "eastus"
+  resource_group_name = "${azurerm_resource_group.terraformdemo.name}"
+
+}
