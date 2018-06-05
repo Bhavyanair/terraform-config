d549624e741eb631a9f9520b402739fff534b44b
root <root@jenkinstestserver.jzipz0o3rtcetgvjxuimt33jkd.tx.internal.cloudapp.net>
2018-06-05 08:52:44 +0000

    subnet and ip

--- main.tf
+++ main.tf
@@ -14,6 +14,20 @@ provider "azurerm" { }
 
 }
 
+  resource "azurerm_subnet" "subnet" {
+  name                 = "terraformjenkinssubnet"
+  resource_group_name  = "${azurerm_resource_group.terraformdemo.name}"
+  virtual_network_name = "${azurerm_virtual_network.network.name}"
+  address_prefix       = "172.50.10.0/24"
+ }
+
+  resource "azurerm_public_ip" "publicip" {
+  name                 = "terraformjenkinspublicip"
+  location             = "eastus"
+  resource_group_name  = "${azurerm_resource_group.terraformdemo.name}"
+  public_ip_address_allocation = "Static"
+
+}
 
 
 
