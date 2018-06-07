96689cd0067e80dcc1c5fe51295f74b60bb8b909
Nair <Bhavya>
2018-06-07 21:18:40 +0530

    remove vm

--- main.tf
+++ main.tf
@@ -31,40 +31,4 @@ resource "azurerm_network_interface" "test" {
   }
 }
 
-resource "azurerm_virtual_machine" "test" {
-  name                  = "mastervm"
-  location              = "${azurerm_resource_group.test.location}"
-  resource_group_name   = "${azurerm_resource_group.test.name}"
-  network_interface_ids = ["${azurerm_network_interface.test.id}"]
-  vm_size               = "Standard_DS1_v2"
-
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-  
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  
-  os_profile {
-    computer_name  = "jenkinsterraformdemo"
-    admin_username = "jenkinsadmin"
-    admin_password = "Password@1234!"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
-  tags {
-    environment = "staging"
-  }
-}
 
