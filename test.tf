9ee97bb89981636316dad1424df3670393954939
Bhavyanair <bhavyanair@kpmg.com>
2018-06-07 21:32:16 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,15 +1,14 @@
 provider "azurerm" { }
 
-resource "azurerm_resource_group" "test" {
+resource "azurerm_resource_group" "test2" {
   name     = "Bhavya"
   location = "East US"
 
 }
 
-
-resource "azurerm_virtual_network" "test" {
+resource "azurerm_virtual_network" "test2" {
   name                = "terraformtestvn"
-  address_space       = ["172.60.0.0/16"]
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
+  address_space       = ["172.70.0.0/16"]
+  location            = "${azurerm_resource_group.test2.location}"
+  resource_group_name = "${azurerm_resource_group.test2.name}"
 }
