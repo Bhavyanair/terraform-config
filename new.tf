ebe0f7dd3f454d2cf21097a49fe31f5ffd036ff7
Nair <Bhavya>
2018-06-08 12:29:14 +0530

    testing

--- new.tf
+++ new.tf
@@ -1,14 +1,14 @@
 provider "azurerm" { }
 
-resource "azurerm_resource_group" "terraformtest" {
-  name     = "terraformrgtest"
+resource "azurerm_resource_group" "new" {
+  name     = "terraformrgnew"
   location = "South India"
 }
 
-resource "azurerm_virtual_network" "terraformtest" {
-  name                = "terraformrgtestvn"
+resource "azurerm_virtual_network" "new" {
+  name                = "terraformrgnewvn"
   address_space       = ["172.10.0.0/16"]
-  location            = "${azurerm_resource_group.terraformtest.location}"
-  resource_group_name = "${azurerm_resource_group.terraformtest.name}"
+  location            = "${azurerm_resource_group.new.location}"
+  resource_group_name = "${azurerm_resource_group.new.name}"
 }
 
