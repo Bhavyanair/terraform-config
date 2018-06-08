79fd6ed12c142fcaf1a7c5f2cd2bfc52164e8516
Nair <Bhavya>
2018-06-08 12:14:41 +0530

    test

--- main.tf
+++ main.tf
@@ -1,34 +1,13 @@
 provider "azurerm" { }
 
-resource "azurerm_resource_group" "test" {
-  name     = "terraformtestrg"
+resource "azurerm_resource_group" "terraformtest" {
+  name     = "terraformrgtest"
   location = "South India"
 }
 
-resource "azurerm_virtual_network" "test" {
-  name                = "terraformtestvn"
-  address_space       = ["172.60.0.0/16"]
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
+resource "azurerm_virtual_network" "terraformtest" {
+  name                = "terraformrgtestvn"
+  address_space       = ["172.10.0.0/16"]
+  location            = "${azurerm_resource_group.terraformtest.location}"
+  resource_group_name = "${azurerm_resource_group.terraformtest.name}"
 }
-
-resource "azurerm_subnet" "test" {
-  name                 = "terraformtestsub"
-  resource_group_name  = "${azurerm_resource_group.test.name}"
-  virtual_network_name = "${azurerm_virtual_network.test.name}"
-  address_prefix       = "172.60.10.0/24"
-}
-
-resource "azurerm_network_interface" "test" {
-  name                = "terraformtestnic"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-
-  ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = "${azurerm_subnet.test.id}"
-    private_ip_address_allocation = "dynamic"
-  }
-}
-
-
